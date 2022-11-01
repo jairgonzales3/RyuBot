3e16116b08aa2a5808a84a9305d5acc9da31de67
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2022-10-31 20:27:50 -0500

    Update info-gruposofc.js

--- Miku/info-gruposofc.js
+++ Miku/info-gruposofc.js
@@ -3,34 +3,30 @@ var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.present
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let texto1 = `
 ╭─────────────┈⊷
-│◦➛☘️✨𝐌𝐈𝐊𝐔_𝐁𝐎𝐓💫
+│◦➛@ffxryu.18
 ╰┬────────────┈⊷
 ┌┤๖ۣۜۜ͜͡𝐇𝐨𝐥𝐚ঔৣֳ᷌᷈͜͡ 💖✨ HOLA TE ENVIAMOS 
 │◦➛☃️️ᩭ✎UNIRTE AL LOS💖 GRUPOS 
 │◦➛🌹️ᩭ✎OFICIALES DEL BOT DE👩‍🏫✨
-│◦➛🍃️ᩭ✎𝐇𝐀𝐃𝐄𝐒 _𝐁𝐎𝐓 y miku bot 
+│◦➛🍃️@ffxryu.18
 │◦➛📚️ᩭ✎ SOMOS UNA FAMILIA DE BOTS 
-│◦➛🌆ᩭ✎𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃 𝚈 𝙼𝙸𝙺𝚄-𝙱𝙾𝚃1 
-│◦➛🍀ᩭ✎https://chat.whatsapp.com/CG5ZPcJ22fL7QjNRzjguD0
-│◦➛☘️ᩭ✎𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃 𝚈 𝙼𝙸𝙺𝚄-𝙱𝙾𝚃2 
-│◦➛✨ᩭ✎https://chat.whatsapp.com/DDm7HC6e5MF9qcdLqB22RQ
-│◦➛📚ᩭ✎𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃 𝚈 𝙼𝙸𝙺𝚄-𝙱𝙾𝚃3 
-│◦➛❤️ᩭ✎https://chat.whatsapp.com/KnpPbr8BN4VDLtwJFMNTtw
-│◦➛🌱ᩭ✎𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃 𝚈 𝙼𝙸𝙺𝚄-𝙱𝙾𝚃4 
-│◦➛📔ᩭ✎https://chat.whatsapp.com/BtzGZregRxzFswm0FgunHL
-│◦➛🌱𝐎𝐅𝐂 𝐘𝐎𝐕𝐀𝐍𝐈☘️ 
+│◦➛🌆𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐆𝐫𝐚𝐭𝐢𝐬𝟏
+│◦➛🍀ᩭ✎https://chat.whatsapp.com/BOAp22xLodrAvxbLG0AD4z
+│◦➛☘️𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐆𝐫𝐚𝐭𝐢𝐬𝟐
+│◦➛✨ᩭ✎https://chat.whatsapp.com/LuHSCjQuCe8G5ucRCQxkJr
+│◦➛@ffxryu.18
 ╰────────────┈⊷`
 let buttonMessage= {
 'document': { url: `https://youtu.be/4eA5nDxPjOA` },
 'mimetype': `application/${document}`,
-'fileName': `📚𝐌𝐈𝐊𝐔_𝐁𝐎𝐓☘️`,
+'fileName': `@ffxryu.18`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'https://github.com/Yovanihades1212/MIKU_BOT.git',
+'mediaUrl': 'https://github.com/jairgonzales3/RyuBot',
 'mediaType': 2,
 'previewType': 'pdf',
 'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
